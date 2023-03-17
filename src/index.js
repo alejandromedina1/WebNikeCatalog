15603f5612852612dd0e1f1d87aac2c4dddf91b1
Isaroru <isabella.rodriguez2@u.icesi.edu.co>
2023-03-16 21:29:27 -0500

    primera parte mobile corregida

--- src/index.js
+++ src/index.js
@@ -1,4 +1,11 @@
 window.addEventListener("scroll", function () {
-    var header = this.document.querySelector("header");
+    var header = this.document.getElementById("desktop-menu");
     header.classList.toggle("abajo", this.window.scrollY>0);
 })
+
+window.addEventListener("scroll", function () {
+    var header = this.document.getElementById("mobile-menu");
+    header.classList.toggle("abajo", this.window.scrollY>0);
+})
+
+var swiper = new Swiper(".mySwiper", {});
