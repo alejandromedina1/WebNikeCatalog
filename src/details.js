cb67305204e944e386865b2031f1b2bb360360e2
alejandromedina1 <alejandro.medina1@u.icesi.edu.co>
2023-04-28 01:57:13 -0500

    Header and footer to detail product

--- src/details.js
+++ src/details.js
@@ -69,3 +69,13 @@ function updateItem(item, key, value) {
     item[key] = value
     item.setAttribute(key, value)
 }
+
+window.addEventListener("scroll", function () {
+    var header = this.document.getElementById("desktop-menu");
+    header.classList.toggle("abajo", this.window.scrollY>0);
+})
+
+window.addEventListener("scroll", function () {
+    var header = this.document.getElementById("mobile-menu");
+    header.classList.toggle("abajo", this.window.scrollY>0);
+})
